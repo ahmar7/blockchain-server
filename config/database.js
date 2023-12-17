e2d71084b8c98fa3a5f94d2aeafd2a16dcf13cce
ahmar7 <ahmarjabbar7@gmail.com>
2023-12-17 08:36:17 +0500

    up

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const database = () => {
-  mongoose.connect(process.env.DATABASE).then((data) => {
-    console.log(`Db connected successfully with ${data.connection.host}`);
-  });
+  mongoose
+    .connect(
+      "mongodb+srv://ahmarjabbar7:Ahmar123@blockchain.4rjskdk.mongodb.net/blockchain"
+    )
+    .then((data) => {
+      console.log(`Db connected successfully with ${data.connection.host}`);
+    });
 };
 
 module.exports = database;
