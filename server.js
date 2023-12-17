4eeb963c36853b2d3d619ceb596896358850c2ee
ahmar7 <ahmarjabbar7@gmail.com>
2023-12-17 08:29:29 +0500

    update

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get("/", async (req, res) => {
   res.send("working");
   console.log("working");
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(errorMiddleware);
 let server = app.listen(process.env.PORT, () => {
   console.log(`server is running at ${process.env.PORT}`);
