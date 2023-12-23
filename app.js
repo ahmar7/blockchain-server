153e751c5a58464199be14012644f8418d90061d
ahmar7 <ahmarjabbar7@gmail.com>
2023-12-23 05:54:32 +0500

    update

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use((req, res, next) => {
   );
 
   res.header("Access-Control-Allow-Credentials", true);
+
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   next();
 });
 //
