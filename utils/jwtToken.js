1d1e8b9d03b5ddab382b2511179916c7848c45d8
ahmar7 <ahmarjabbar7@gmail.com>
2024-01-02 05:12:32 +0500

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,9 +2,8 @@
 const jwtToken = (user, statusCode, res) => {
   let token = user.generateToken();
   let options = {
-    expires: new Date(
-      Date.now() + process.env.TOKEN_EXPIRE * 24 * 60 * 60 * 1000
-    ),
+    // expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    maxAge: process.env.TOKEN_EXPIRE * 24 * 60 * 60,
     httpOnly: true,
     sameSite: "None",
     secure: true,
