91beaf554abc86d30870cf849062c1560eeafa06
ahmar7 <ahmarjabbar7@gmail.com>
2024-01-07 03:48:24 +0500

    update

--- config/database.js
+++ config/database.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const database = () => {
   mongoose
-    .connect(
-      "mongodb+srv://ahmarjabbar7:Ahmar123@blockchain.4rjskdk.mongodb.net/blockchain"
-    )
+    .connect(process.env.DATABASE)
     .then((data) => {
       console.log(`Db connected successfully with ${data.connection.host}`);
     })
